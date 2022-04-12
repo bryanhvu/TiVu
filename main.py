40aebec46470743c4a837ab4a31f90542de32a96
bryanhvu <bryanhvu5@gmail.com>
2022-04-12 13:12:00 -0500

    Setup basic project requirements

---
+++ main.py
@@ -0,0 +1,7 @@
+#TODO Setup Google Sheets
+
+#TODO Interface with sheets using Sheety API
+
+#TODO Automate starting up and playing specified youtube videos
+
+#TODO Play videos listed in sheets based on day/other criteria
